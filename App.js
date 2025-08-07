b289967c78900a3e8e33641683df1025c42cd661
Dann Martinez <dannstark@hotmail.com>
2025-08-06 23:45:18 -0600

    "Funciones para tareas y evitar repeticiones"

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text,View, TextInput, Button} from 'react-native';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [userInput, setUserInput] = useState("");
+  const [tasksList,setTasksList] = useState([]);
+
+  console.log("User Input:", userInput)
+  console.log("Tasks List:", tasksList) 
+
+  const handleAddTask = ()=>{
+    // Verifica si ya existe una tarea con el mismo texto
+    const exists =tasksList.some(task => task.text === userInput);
+    if (exists) {
+      alert("Esta tarea ya existe.");
+      return;
+    }
+    setTasksList([...tasksList, {id: Math.random().toString(), text: userInput}]);
+  }
+
+
   return (
     <View style={styles.container}>
       <View style={styles.taskInputContainer}>
       <TextInput style={styles.taskInput}
-      onChangeText={()=>{}}
+      //Funcion de una sola linea ↓
+      onChangeText={(text)=>setUserInput(text)}
       placeholder="Qué quieres hacer hoy?"
       />
-      <Button title="Add" onPress={()=>{}} />  
+      <Button title="Add" onPress={handleAddTask} />  
       </View>
+
+//Lista de tareas
+      <View style = {styles.tasksListContainer} />
+      {
+        
+        tasksList.map((task, index) => (
+          <Text key={task.id}>{task.text}</Text>
+        ))
+      }
       <StatusBar style="auto"/>
     </View>
   );
 }
 
+
+//Estilos de página
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -35,5 +66,9 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     paddingHorizontal: 16,
     fontSize: 16
+  },
+  tasksListContainer: {
+    paddingHorizontal: 16,
+    gap: 8,
   },
 });
